193db080ef031237916c4d8aa0a20906e55d9f75
Weronika Tarnawska <weratarnawska@gmail.com>
2023-12-10 12:39:53 +0100

    added shopping list and chilli con carne recipie :)

---
+++ src/evaluator.rs
@@ -0,0 +1,61 @@
+use crate::parser::{Command, Expr};
+
+pub fn eval(cmd: Command) {
+    match cmd {
+        Command::Forward(expr) => forward(eval_expr(expr)),
+        _ => unimplemented!(),
+    }
+}
+
+fn eval_expr(expr: Expr) -> f32 {
+    8.0
+}
+
+fn forward(dist: f32) {}
+
+/*
+Lista Zakupów:
+
+* cebula
+* kmin rzymmski
+* kolendra
+* wołowina mielona
+* pomidory (do concarne + do śniadania = dużo)
+* papryka
+* fasola w puszce
+
+* mleko
+* bułki
+* coś do smarowania, np pasztet <3, lub paprykarz szczeciński lub smalec (hummus i mascarpone jest)
+* cukier albo miód do herbaty
+
+* budyń (śmietankowy/waniliowy)
+* kisiel o fajnym smaku (nie truskawkowy, nie żółty, czerwony/granatowy ok)
+* pistacje (możesz ich nie brać, bo do niczego nie pasują)
+*/
+
+/*
+Przepis na chili con carne:
+Skłądniki:
+1 łyżka oliwy
+1 cebula
+2 ząbki czosnku
+1 łyżeczka chilli w proszku
+1 łyżeczka zmielonego kminu rzymskiego (kuminu)
+1 łyżeczka suszonego oregano
+500 g mielonej wołowiny (np. gulaszowej, antrykotu)
+2 świeże pomidory lub 2/3 puszki obranych pomidorów
+1 łyżeczka cukru
+1 czerwona papryka
+200 g czerwonej fasoli (ugotowanej)
+około 100 ml bulionu np. wołowego lub wody
+
+Przygotowanie:
+(kolejność wrzucania na patelnię)
+* cebula + przyprawy
+* wołowina
+* pomidory
+* papryka
+* fasola
+* bulion
+*/
